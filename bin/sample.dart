b2c5fbe35a28b37c2c9335e66cae393d96618f69
zamna <ayshazamna123@gmail.com>
2024-09-20 11:13:05 +0530

    commit

---
+++ bin/sample.dart
@@ -0,0 +1,11 @@
+main(){
+  print("Welcome");
+  print(12);
+  print(10.9);
+  print(true);
+  print("Aysha Zamna");
+  print(21);
+  print("flutter");
+  print(false);
+
+}
---
+++ bin/variables.dart
@@ -0,0 +1,16 @@
+void main(){
+  String name="Zamna";
+  print(name);
+  print("My name is $name");
+  int age=21;
+  print("my age is $age");
+  print(age);
+  double mark=25.5;
+  print("my mark is $mark");
+  int a=20;
+  int b=30;
+  int sum=a+b;
+  print("sum=$sum");
+
+
+}
